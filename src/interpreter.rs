7df28500d986fda2f952f778b8d3b1c6efeadfa6
KarkiAdit <adityakarki728@gmail.com>
2025-02-14 18:44:27 -0600

    implement eval and parsing logic for adder interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -8,6 +8,7 @@ pub fn eval(e: &Expr) -> i32 {
         Expr::Num(n) => *n,
         Expr::Add1(e1) => eval(e1) + 1,
         Expr::Sub1(e1) => eval(e1) - 1,
+        Expr::Negate(e1) => -eval(e1),
     }
 }
 
@@ -19,7 +20,8 @@ pub fn parse_expr(s: &Sexp) -> Expr {
             match &vec[..] {
                 [Sexp::Atom(S(op)), e] if op == "add1" => Expr::Add1(Box::new(parse_expr(e))),
                 [Sexp::Atom(S(op)), e] if op == "sub1" => Expr::Sub1(Box::new(parse_expr(e))),
-                _ => panic!("parse error"), // Keeping this as per the assignment
+                [Sexp::Atom(S(op)), e] if op == "negate" => Expr::Negate(Box::new(parse_expr(e))), 
+                _ => panic!("parse error"),
             }
         },
         _ => panic!("parse error"),
@@ -30,11 +32,24 @@ pub fn parse_expr(s: &Sexp) -> Expr {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use sexp::{Sexp, Atom::*};
 
     #[test]
     fn test_eval_num() {
         let expr = Expr::Num(10);
         assert_eq!(eval(&expr), 10);
+    }
+
+    #[test]
+    fn test_eval_zero() {
+        let expr = Expr::Num(0);
+        assert_eq!(eval(&expr), 0);
+    }
+
+    #[test]
+    fn test_eval_negative_num() {
+        let expr = Expr::Num(-5);
+        assert_eq!(eval(&expr), -5);
     }
 
     #[test]
@@ -56,6 +71,30 @@ mod tests {
     }
 
     #[test]
+    fn test_eval_negate() {
+        let expr = Expr::Negate(Box::new(Expr::Num(5))); // negate(5) => -5
+        assert_eq!(eval(&expr), -5);
+    }
+
+    #[test]
+    fn test_eval_double_negate() {
+        let expr = Expr::Negate(Box::new(Expr::Negate(Box::new(Expr::Num(4))))); // negate(negate(4)) => 4
+        assert_eq!(eval(&expr), 4);
+    }
+
+    #[test]
+    fn test_eval_negate_add1() {
+        let expr = Expr::Negate(Box::new(Expr::Add1(Box::new(Expr::Num(3))))); // negate(add1(3)) => -4
+        assert_eq!(eval(&expr), -4);
+    }
+
+    #[test]
+    fn test_eval_add1_negate() {
+        let expr = Expr::Add1(Box::new(Expr::Negate(Box::new(Expr::Num(2))))); // add1(negate(2)) => -1
+        assert_eq!(eval(&expr), -1);
+    }
+
+    #[test]
     fn test_parse_add1() {
         let sexpr = Sexp::List(vec![Sexp::Atom(S("add1".to_string())), Sexp::Atom(I(5))]);
         let parsed = parse_expr(&sexpr);
@@ -67,6 +106,27 @@ mod tests {
         let sexpr = Sexp::List(vec![Sexp::Atom(S("sub1".to_string())), Sexp::Atom(I(7))]);
         let parsed = parse_expr(&sexpr);
         assert_eq!(parsed, Expr::Sub1(Box::new(Expr::Num(7))));
+    }
+
+    #[test]
+    fn test_parse_negate() {
+        let sexpr = Sexp::List(vec![Sexp::Atom(S("negate".to_string())), Sexp::Atom(I(9))]);
+        let parsed = parse_expr(&sexpr);
+        assert_eq!(parsed, Expr::Negate(Box::new(Expr::Num(9))));
+    }
+
+    #[test]
+    fn test_parse_negate_nested() {
+        let sexpr = Sexp::List(vec![Sexp::Atom(S("negate".to_string())), Sexp::List(vec![Sexp::Atom(S("add1".to_string())), Sexp::Atom(I(2))])]);
+        let parsed = parse_expr(&sexpr);
+        assert_eq!(parsed, Expr::Negate(Box::new(Expr::Add1(Box::new(Expr::Num(2))))));
+    }
+
+    #[test]
+    fn test_parse_add1_negate() {
+        let sexpr = Sexp::List(vec![Sexp::Atom(S("add1".to_string())), Sexp::List(vec![Sexp::Atom(S("negate".to_string())), Sexp::Atom(I(2))])]);
+        let parsed = parse_expr(&sexpr);
+        assert_eq!(parsed, Expr::Add1(Box::new(Expr::Negate(Box::new(Expr::Num(2))))));
     }
 
     #[test]
